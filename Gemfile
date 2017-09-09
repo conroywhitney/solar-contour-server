24e7e3c1aca9282c9fd163b928ee2feff5e32554
Conroy Whitney <conroy.whitney@gmail.com>
2017-09-09 13:36:50 -0800

    Add rack cors

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
+gem "rack-cors", :require => "rack/cors"
 gem "rack-reverse-proxy", require: "rack/reverse_proxy"
 gem "sinatra"
--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
-require "sinatra"
+require "rack/cors"
 require "rack/reverse_proxy"
+require "sinatra"
+
+use Rack::Cors do
+  allow do
+    origins "*"
+    resource "*", headers: :any, methods: :get
+  end
+end
 
 use Rack::ReverseProxy do
   reverse_proxy /^\/(.+)$/, 'https://ukwpiuisi9.execute-api.us-west-2.amazonaws.com/$1'
